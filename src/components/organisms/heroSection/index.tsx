c84a950732d19e993382f868fb82ba87ffaed14e
Jim Bailey <jbailey@ukcloud.com>
2019-08-20 12:01:50 +0100

    Make hero section nice

--- src/components/organisms/heroSection/index.tsx
+++ src/components/organisms/heroSection/index.tsx
@@ -3,12 +3,16 @@ import SaveTheDate from '../../molecules/saveTheDate'
 import './styles.scss'
 
 interface IHeroSectionProps {
+  backgroundImage: string
   centralWidget: typeof SaveTheDate
 }
 
 function HeroSection(props: IHeroSectionProps) {
   return (
-    <div className="heroSection">
+    <div
+      className="heroSection"
+      style={{ backgroundImage: `url(${props.backgroundImage})` }}
+    >
     </div>
   )
 }
--- src/pages/front/index.tsx
+++ src/pages/front/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LongScroll from '../../components/templates/longScroll';
 import HeroSection from '../../components/organisms/heroSection';
 import SaveTheDate from '../../components/molecules/saveTheDate';
+import LeavesBackground from '../../assets/img/leaves.jpg'
 
 function saveTheDate() {
   return(<SaveTheDate/>)
@@ -12,7 +13,10 @@ function FrontPage() {
     <div className="main">
       <LongScroll>
         <div className="App-content">
-          <HeroSection centralWidget={saveTheDate}/>
+          <HeroSection
+            backgroundImage={LeavesBackground}
+            centralWidget={saveTheDate}
+          />
         </div>
       </LongScroll>
     </div>
