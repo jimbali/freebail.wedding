28d3631e37b388633d5fcd242f1e03cccb8b19c4
Jim Bailey <bailey.jim@outlook.com>
2019-08-20 17:37:38 +0100

    Add nice font

--- src/components/molecules/saveTheDate/index.tsx
+++ src/components/molecules/saveTheDate/index.tsx
@@ -4,10 +4,12 @@ import './styles.scss'
 function SaveTheDate() {
   return (
     <div className="saveTheDate">
-        <h2>Sarah & Jim</h2>
-        <p>are getting</p>
-        <p>married!</p>
-        <p>24 Oct 2020</p>
+        <div className="inner">
+            <h2>Sarah & Jim</h2>
+            <p>are getting</p>
+            <p>married!</p>
+            <p>24 Oct 2020</p>
+        </div>
     </div>
   )
 }
