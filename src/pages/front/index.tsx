81a1799c36cca5341fb69f1cd58a08231b6542e7
Jim Bailey <bailey.jim@outlook.com>
2019-09-02 23:13:40 +0100

    Get guest from API

--- src/pages/front/index.tsx
+++ src/pages/front/index.tsx
@@ -9,24 +9,61 @@ interface IFrontPageRouteParams {
   inviteCode: string
 }
 
-function FrontPage(props: RouteComponentProps<IFrontPageRouteParams>) {
-  const inviteCode: string = props.match.params.inviteCode
+interface IFrontPageState {
+  guest: IGuest
+}
 
-  return (
-    <div className="main">
-      <LongScroll>
-        <div className="App-content">
-          <HeroSection
-            backgroundImage={LeavesBackground}
-            centralWidget={<SaveTheDate/>}
-          />
-          <div>
-            {inviteCode}
+interface IGuest {
+  name: string
+}
+
+class FrontPage extends React.Component<RouteComponentProps<IFrontPageRouteParams>, IFrontPageState> {
+  constructor(props: RouteComponentProps<IFrontPageRouteParams>) {
+    super(props);
+    this.state = { guest: { name: 'hello' } }
+  }
+
+  requestUrl(): string {
+    return '/api/guests/' + this.props.match.params.inviteCode
+  }
+
+  updateFormFields() {
+    new Promise<IGuest>((resolve, reject) => {
+      fetch(this.requestUrl())
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            console.log(response)
+          }
+        })
+        .then(jsonResponse => {
+          resolve(jsonResponse as IGuest)
+        })
+    }).then((guest: IGuest) => { console.log(guest); this.setState({ guest: guest }) })
+  }
+
+  componentDidMount() {
+    this.updateFormFields()
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <LongScroll>
+          <div className="App-content">
+            <HeroSection
+              backgroundImage={LeavesBackground}
+              centralWidget={<SaveTheDate/>}
+            />
+            <div>
+              {this.state.guest.name}
+            </div>
           </div>
-        </div>
-      </LongScroll>
-    </div>
-  );
+        </LongScroll>
+      </div>
+    );
+  }
 }
 
 export default FrontPage;
